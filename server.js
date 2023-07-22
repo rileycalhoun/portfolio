7c1edf3188840501a6a1672eb9bb657327149261
Riley Calhoun <rileycalhoun0228@gmail.com>
2023-07-22 15:57:38 -0700

    Port variable

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
 import { handler } from './build/handler.js';
 import express from 'express';
+
+dotenv.config();
+let port = process.env.PORT || 3000;
 
 const app = express();
 
 app.get('/health', (req, res) => res.end("OK"));
 app.use(handler);
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
